64fc3e393fa69ae69a6a2e3bce005cc46de838b3
agamatlab <icloudakberzade@gmail.com>
2025-03-02 17:32:52 -0500

    add: demo

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TarodevController;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -15,6 +16,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var PlayerStats = gameObject.GetComponent<PlayerController>();
+        PlayerStats._stats.MaxSpeed = 42;
+        PlayerStats._stats.Acceleration= 30;
+        PlayerStats._stats.JumpPower = 25;
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
